e3f71d38359ff6dccc3de76412bd0c143296c5b2
nate-d-olson <nolson@nist.gov>
2018-06-20 15:53:47 -0400

    debugging

--- NISTv3.3.2/DNAnexusCommands/batch_processing_commands/Batch_Processing_shell_scritps_GRCh38/GRCh38/HG006_and_HG007/giab_complete_genomics_pipeline.sh
+++ NISTv3.3.2/DNAnexusCommands/batch_processing_commands/Batch_Processing_shell_scritps_GRCh38/GRCh38/HG006_and_HG007/giab_complete_genomics_pipeline.sh
@@ -21,7 +21,8 @@ while [ "$1" != "" ]; do
                             VCFURL=$1
                             ;;
         --cgvcf )           shift
-                            cgvcf=$1
+                            CGVCF=$1
+                            ;;
         --refid )           shift
                             REFID=$1
                             ;;
@@ -40,7 +41,8 @@ while [ "$1" != "" ]; do
         # -h | --help )       usage
         #                     exit
         #                     ;;
-        * )                 OTHER=$1 #usage
+        * )                 shift
+                            echo $1 #usage
                             exit 1
     esac
     shift
@@ -53,14 +55,15 @@ INPUTVCF=${ROOTDIR}/${CGVCF}
 ################################################################################
 ############## Upload data to DNAnexus
 
-UPLOADJOBID=$(dx run url_fectcher -iurl=${VCFURL} -ioutput_name=${INPUTVCF})
+UPLOADJOBID=$(dx run -y url_fetcher -iurl=${VCFURL})
 
+MOVEJOBID=$(dx wait ${UPLOADJOBID} && dx mv ${UPLOADJOBID}:file ${INPUTVCF})
 ################################################################################
 ############## run integration prepare CG
 
 ## Use array for saving jobids
 declare -a PREPJOBIDS
-for i in {1 .. 22} X Y;
+for i in {1..22} X Y;
   do
     JOBID=$(dx wait ${UPLOADJOBID} && \
       dx run -y \
@@ -77,15 +80,17 @@ done
 ## Combining input values
 DEPENDIDS=""
 CHROMVCFS=""
-CHROMCALLABLEBEDS=""
-CHROMNOTCALLABLEBEDS=""
-for i in PREPJOBIDS;
+CHROMBEDS=""
+for i in ${PREPJOBIDS[@]};
   do
     DEPENDIDS="${DEPENDIDS} --depends-on  ${i}"
     CHROMVCFS="${CHROMVCFS} -ivcfs=${i}:outvcfgz"
-    CHROMCALLABLEBEDS="${CHROMCALLABLEBEDS} -ibeds==${i}:outcallablebed"
+    CHROMBEDS="${CHROMBEDS} -ibeds==${i}:outcallablebed"
   done
 
+echo ${DEPENDIDS}
+echo ${CHROMVCFS}
+echo #{CHROMBEDS}
 ## Output prefix
 COMBINEDPREFIX=${HG}_GRCh37_CHROM1-Y_${CGVCF}
 
@@ -132,62 +137,62 @@ dx wait ${COMBINEDBEDJOBID} && dx download -o ${GRCh37BED} ${COMBINEDBED}
 
 ############## convert CHROM number for GRCh37 highconf files
 
-# Parse out vcf header
-grep ^# ${GRCh37VCF} > ${GRCh37HEADER}
+# # Parse out vcf header
+# grep ^# ${GRCh37VCF} > ${GRCh37HEADER}
 
-#create no header file
-sed '/^#/ d' ${GRCh37VCF} > ${GRCh37NOHEADER}
+# #create no header file
+# sed '/^#/ d' ${GRCh37VCF} > ${GRCh37NOHEADER}
 
-# replace chrom # with chr# at start of every row in no header file
-sed 's/^/chr/' ${GRCh37NOHEADER}  > ${GRC37NOHEADERCHROMFIX}
+# # replace chrom # with chr# at start of every row in no header file
+# sed 's/^/chr/' ${GRCh37NOHEADER}  > ${GRC37NOHEADERCHROMFIX}
 
-# replace chrom # with chr# at start of every row in bed
-sed 's/^/chr/' ${GRCh37BED} > ${GRCh37BEDCHROMFIX}
+# # replace chrom # with chr# at start of every row in bed
+# sed 's/^/chr/' ${GRCh37BED} > ${GRCh37BEDCHROMFIX}
 
-# add header back in
-cat ${GRCh37HEADER} ${GRCh37NOHEADERCHROMFIX} > ${GRCH37VCFCHROMFIX}
+# # add header back in
+# cat ${GRCh37HEADER} ${GRCh37NOHEADERCHROMFIX} > ${GRCH37VCFCHROMFIX}
 
-## Cleanup
-rm ${GRCh37VCF} ${GRCh37NOHEADER} ${GRC37NOHEADERCHROMFIX} ${GRCh37BED}
+# ## Cleanup
+# rm ${GRCh37VCF} ${GRCh37NOHEADER} ${GRC37NOHEADERCHROMFIX} ${GRCh37BED}
 
-###### Perform liftover (note: fasta's cannot be zipped)
+# ###### Perform liftover (note: fasta's cannot be zipped)
 
-for i in {1..22} X Y;
-  do
+# for i in {1..22} X Y;
+#   do
 
-    # Separate out each autosome and the X
-    egrep "^(#|(chr)?${i}[[:space:]])" ${GRCh37VCFCHROMFIX} > GRCh37_chr${i}.vcf
-    egrep "^(chr)?${i}[[:space:]]" ${GRCh37BEDCHROMFIX} > GRCh37_chr${i}.bed
+#     # Separate out each autosome and the X
+#     egrep "^(#|(chr)?${i}[[:space:]])" ${GRCh37VCFCHROMFIX} > GRCh37_chr${i}.vcf
+#     egrep "^(chr)?${i}[[:space:]]" ${GRCh37BEDCHROMFIX} > GRCh37_chr${i}.bed
 
-    LIFTOVERVCF=${HG}_CG_chr${i}_LIFTOVER.vcf
-    LIFTOVERBED=${HG}_CG_chr${i}_LIFTOVER.bed
+#     LIFTOVERVCF=${HG}_CG_chr${i}_LIFTOVER.vcf
+#     LIFTOVERBED=${HG}_CG_chr${i}_LIFTOVER.bed
 
 
-    #liftover (note: fasta's cannot be zipped)
-    java -jar -Xmx10g ${GENOMEWARP}/verilylifesciences-genomewarp-1.0.0-runnable.jar \
-      --lift_over_chain_path ${LIFTOVERCHAIN} \
-      --raw_query_vcf GRCh37_chr${i}.vcf \
-      --raw_query_bed GRCh37_chr${i}.bed \
-      --ref_query_fasta  ${QUERYREF}\
-      --ref_target_fasta  ${TARGETREF}\
-      --work_dir ${WORKDIR} \
-      --output_variants_file ${LIFTOVERVCF} \
-      --output_regions_file ${LIFEOVERBED}
+#     #liftover (note: fasta's cannot be zipped)
+#     java -jar -Xmx10g ${GENOMEWARP}/verilylifesciences-genomewarp-1.0.0-runnable.jar \
+#       --lift_over_chain_path ${LIFTOVERCHAIN} \
+#       --raw_query_vcf GRCh37_chr${i}.vcf \
+#       --raw_query_bed GRCh37_chr${i}.bed \
+#       --ref_query_fasta  ${QUERYREF}\
+#       --ref_target_fasta  ${TARGETREF}\
+#       --work_dir ${WORKDIR} \
+#       --output_variants_file ${LIFTOVERVCF} \
+#       --output_regions_file ${LIFEOVERBED}
 
-    #remove extra contigs so only left with chr${i}
-    egrep "^(#|(chr)?${i}[[:space:]])" ${LIFTOVERVCF} > ${convGRCh38VCF}
-    egrep "^(chr)?${i}[[:space:]]" ${LIFTOVERBED} > ${GRCh38BED}
+#     #remove extra contigs so only left with chr${i}
+#     egrep "^(#|(chr)?${i}[[:space:]])" ${LIFTOVERVCF} > ${convGRCh38VCF}
+#     egrep "^(chr)?${i}[[:space:]]" ${LIFTOVERBED} > ${GRCh38BED}
 
-    ## Sort, index, and zip VCF file
-    GRCh38VCF=${HG}_${i}_convGRCh38_CG_${CGVCF}_sorted.vcf.gz
-    GRCh38VCFIDX=${HG}_${i}_convGRCh38_CG_${CGVCF}.vcf.tbi
+#     ## Sort, index, and zip VCF file
+#     GRCh38VCF=${HG}_${i}_convGRCh38_CG_${CGVCF}_sorted.vcf.gz
+#     GRCh38VCFIDX=${HG}_${i}_convGRCh38_CG_${CGVCF}.vcf.tbi
 
-    sed '/^#/ d' ${convGRCh38VCF} | sort -k2,2n | bgzip -c > ${GRCh38VCF}
-    tabix -f ${GRCh38VCF}
+#     sed '/^#/ d' ${convGRCh38VCF} | sort -k2,2n | bgzip -c > ${GRCh38VCF}
+#     tabix -f ${GRCh38VCF}
 
-    ## upload liftover vcfs and bed to DNAnexus
-    dx upload --destination ${ROOTDIR} ${GRCh38VCF} ${GRCh38VCFIDX} ${GRCh38BED}
+#     ## upload liftover vcfs and bed to DNAnexus
+#     dx upload --destination ${ROOTDIR} ${GRCh38VCF} ${GRCh38VCFIDX} ${GRCh38BED}
 
-    ## Cleanup
-    rm ${LIFTOVERVCF} ${LIFEOVERBED} ${convGRCh38VCF}
-done
+#     ## Cleanup
+#     rm ${LIFTOVERVCF} ${LIFEOVERBED} ${convGRCh38VCF}
+# done
--- NISTv3.3.2/DNAnexusCommands/batch_processing_commands/Batch_Processing_shell_scritps_GRCh38/GRCh38/HG006_and_HG007/run_complete_genomics_chinese_parents.sh
+++ NISTv3.3.2/DNAnexusCommands/batch_processing_commands/Batch_Processing_shell_scritps_GRCh38/GRCh38/HG006_and_HG007/run_complete_genomics_chinese_parents.sh
@@ -2,7 +2,7 @@
 ## Running the Complete genomics pipelines for Chinese Parents
 
 ## Common parameters
-LIFTOVERCHAIN=../Liftover_scripts/hg19ToHg38.over.chain
+LIFTOVERCHAIN=hg19ToHg38.over.chain
 QUERYREF=ucsc.hg19.fasta
 TARGETREF=GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna
 GENOMEWARP=/scratch/nolson/giab/genomewarp/target
@@ -14,15 +14,15 @@ bash giab_complete_genomics_pipeline.sh \
     --cgvcf vcfBeta-GS000037476-ASM \
     --liftover ${LIFTOVERCHAIN} \
     --queryref ${QUERYREF} \
-    --tergetref ${TARGETREF} \
+    --targetref ${TARGETREF} \
     --genomewarp ${GENOMEWAPR}
 
 ## HG007 Mother
-bash giab_complete_genomics_pipeline.sh \
-    --hg HG007 \
-    --vcfurl ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/ChineseTrio/analysis/CompleteGenomics_HanTrio_ExtractedFromCoriellCells_SmallVariants_CGAtools_08082014/HG007_NA24695-hu38168_mother/ASM/vcfBeta-GS000037477-ASM.vcf.bz2 \
-    --cgvcf vcfBeta-GS000037477-ASM \
-    --liftover ${LIFTOVERCHAIN} \
-    --queryref ${QUERYREF} \
-    --tergetref ${TARGETREF} \
-    --genomewarp ${GENOMEWAPR}
+# bash giab_complete_genomics_pipeline.sh \
+#     --hg HG007 \
+#     --vcfurl ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/ChineseTrio/analysis/CompleteGenomics_HanTrio_ExtractedFromCoriellCells_SmallVariants_CGAtools_08082014/HG007_NA24695-hu38168_mother/ASM/vcfBeta-GS000037477-ASM.vcf.bz2 \
+#     --cgvcf vcfBeta-GS000037477-ASM \
+#     --liftover ${LIFTOVERCHAIN} \
+#     --queryref ${QUERYREF} \
+#     --targetref ${TARGETREF} \
+#     --genomewarp ${GENOMEWAPR}
